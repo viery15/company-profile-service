1ddcbab0e099d1b062bb746efd29832f759e3182
Viery Darmawan <vierydarmawan2@gmail.com>
2023-05-18 11:02:41 +0700

    update config

--- config/database.php
+++ config/database.php
@@ -45,6 +45,24 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_CASE => PDO::CASE_NATURAL,
+            ]) : [],
+        ],
+
+        'mysql_local' => [
+            'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
