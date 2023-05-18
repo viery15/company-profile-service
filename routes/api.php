faefba5abba9a97a818d2249e054af74002ed7bb
Viery Darmawan <vierydarmawan2@gmail.com>
2023-05-18 10:06:18 +0700

    disable jwt

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'jwt.auth'], function () {
+// Route::group(['middleware' => 'jwt.auth'], function () {
     Route::get('/users', [UserController::class, 'findAll']);
     Route::post('/users', [UserController::class, 'store']);
 
@@ -14,6 +14,6 @@ Route::group(['middleware' => 'jwt.auth'], function () {
 
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::get('/categories', [CategoryController::class, 'findAll']);
-});
+// });
 
 Route::post('login', [LoginController::class, 'login']);
