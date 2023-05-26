dab8f33cad0224e2f4abd86ed5d4ced0e7470b01
Viery Darmawan <vierydarmawan2@gmail.com>
2023-05-26 21:08:20 +0700

    update migration script

--- app/Infrastructure/Database/Migrations/2023_05_18_070751_create_configurations_table.php
+++ app/Infrastructure/Database/Migrations/2023_05_18_070751_create_configurations_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('configurations', function (Blueprint $table) {
             $table->id();
-            $table->string('key');
-            $table->string('value')->unique();
+            $table->string('key')->unique();
+            $table->string('label');
+            $table->string('value');
             $table->timestamps();
         });
     }
--- database/seeders/ConfigurationSeeder.php
+++ database/seeders/ConfigurationSeeder.php
@@ -18,26 +18,27 @@ class ConfigurationSeeder extends Seeder
             [
                 [
                     'key' => 'COMPANY_NAME',
+                    'label' => 'Company Name',
                     'value' => 'Taman Dayu Golf Club & Resort'
                 ],
                 [
                     'key' => 'ADDRESS',
+                    'label' => 'Address',
                     'value' => 'Jl. Raya Surabaya-Malang Km.48 Pandaan 67156 Pasuruan, East Java - Indonesia'
                 ],
                 [
                     'key' => 'PHONE_NUMBER',
+                    'label' => 'Phone Number',
                     'value' => '+62 343 674 1234'
                 ],
                 [
                     'key' => 'PHONE_NUMBER_2',
+                    'label' => 'Phone Number 2',
                     'value' => '+62 811 34 88 234'
                 ],
                 [
-                    'key' => 'HOME_VIDEO_URL',
-                    'value' => 'https://www.tamandayu.magnamediadesign.com/assets/video-header.mp4'
-                ],
-                [
                     'key' => 'EMAIL',
+                    'label' => 'Email',
                     'value' => 'info@tamandayu.com'
                 ]
             ]
